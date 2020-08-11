d7b48db75090a492378dab35df764420208c8411
gazoubel <gazoubel@gmail.com>
2020-08-11 14:26:13 -0400

    First commit

---
+++ src/build/precache-manifest.b03925be62ae6864fafa1a1cb6d19dfd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb56a86bbb78a48f49b18e3d809061b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd51c986f3dd23880879",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "33803bca1eb6d23093f9",
+    "url": "/static/js/2.def41a19.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.def41a19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd51c986f3dd23880879",
+    "url": "/static/js/main.cd746aef.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
